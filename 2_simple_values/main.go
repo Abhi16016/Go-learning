af01b4807dff38b2af41a826d492633f016c7df5
Abhi16016 <abhinavkale919913@gmail.com>
2025-01-22 04:04:55 +0530

    Day-1

---
+++ 2_simple_values/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//int
+	fmt.Println(1)
+	//string
+	fmt.Println("Hello go")
+	//boolean
+	fmt.Println(true)
+	fmt.Println(false)
+	//float
+	fmt.Println(19.6)
+}
---
+++ 3_variables/variables.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "GO"
+	fmt.Println(name)
+
+	var isAge bool = false
+	fmt.Println(isAge)
+
+	var assign string
+	assign = "assigned value"
+	fmt.Println(assign)
+
+	var price float32 = 50.5
+	fmt.Println(price)
+
+	//shorthand syntax
+	short := "go-short-hand"
+	fmt.Println(short)
+
+	price2 := 30.33
+	fmt.Println(price2)
+
+}
